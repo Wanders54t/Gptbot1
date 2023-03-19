e990ebdd66e967b391d8066f89eab1d6b0e5e551
WANDERSON <93231003+Wanders54t@users.noreply.github.com>
2023-03-19 13:37:56 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -11,3 +11,13 @@ def hello():
     return "Hello, world!"
 
 app.run(port=int(os.environ.get("PORT", 5000)))
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, world!"
+
+app.run(port=int(os.environ.get("PORT", 5000)))
